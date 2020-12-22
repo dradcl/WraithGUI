b03b4c428cbcf44b011be6e1f2346a2d769c2179
Karma Kitten <53679096+karmakittenx@users.noreply.github.com>
2020-12-21 20:03:03 -0500

    Add files via upload

---
+++ GUIMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Color = UnityEngine.Color;
+
+namespace WraithGUI
+{
+    public sealed class GUIMenu
+    {
+        public static Color[] allColors = { Color.red, Color.yellow, Color.green, Color.cyan, Color.blue, Color.magenta, Color.white, Color.grey, Color.black, };
+        private static float tValue;
+        public static Color RandomColor()
+        {
+            return allColors[UnityEngine.Random.Range(0, allColors.Length)];
+        }
+        public static void CycleColors(GUIStyle guiStyle, bool background =  false)
+        {
+            tValue = (Mathf.Sin(Time.time * 1.5f) + 1) / 2.0f;
+            if (!background)
+            {
+                guiStyle.normal.textColor = Color.Lerp(Color.cyan, Color.magenta, tValue);
+            }
+            else
+            {
+                guiStyle.normal.background = MakeTex(51, 26, Color.Lerp(Color.cyan, Color.magenta, tValue));
+            }
+        }
+        private static Texture2D MakeTex(int width, int height, Color col)
+        {
+            Color[] pix = new Color[width * height];
+            for (int i = 0; i < pix.Length; ++i)
+            {
+                pix[i] = col;
+            }
+            Texture2D result = new Texture2D(width, height);
+            result.SetPixels(pix);
+            result.Apply();
+            return result;
+        }
+    }
+}
---
+++ PhasmoGame.cs
@@ -0,0 +1,125 @@
+﻿// WraithGUI by Karma Kitten
+// For Phasmophobia build #5635423, manifest #4488252479481708564
+
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace WraithGUI
+{
+    public sealed class PhasmoGame : MelonMod
+    {
+        private static readonly GUIStyle GUIStyle = new GUIStyle();
+        private static readonly GUIStyle backgroundStyle = new GUIStyle();
+        private bool menuIsEnabled = false;
+        private bool mainMenuIsEnabled = false;
+        private bool playerMenuIsEnabled = false;
+        private bool ghostMenuIsEnabled = false;
+        private bool levelMenuIsEnabled = false;
+        private int levelNum;
+        private static string[] mapNames = { "Opening Scene", "Lobby", "Tanglewood Street", "Ridgeview Road House", "Edgefield Street House", "Asylum", "Brownstone High School", "Bleasdale Farmhouse" , "Grafton Farmhouse" };
+
+        public static LevelController levelController;
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Log($"Successfully loaded v{typeof(PhasmoGame).Assembly.GetName().Version}. Enjoy!");
+            GUIStyle.alignment = TextAnchor.MiddleCenter;
+            GUIStyle.fontSize = 16;
+            GUIStyle.fontStyle = FontStyle.Bold;
+            backgroundStyle.alignment = TextAnchor.MiddleCenter;
+            backgroundStyle.normal.textColor = Color.white;
+        }
+        public override void OnLevelWasInitialized(int level)
+        {
+            levelNum = level; // Save this for checks later.
+            MelonLogger.Log($"{mapNames[level]} was initialized");
+            InitializeGameObjects(); // This will ensure the level is never null
+        }
+        public override void OnUpdate()
+        {
+            // Open Menu (F1)
+            if (Keyboard.current.f1Key.wasPressedThisFrame)
+            {
+                menuIsEnabled = !menuIsEnabled;
+            }
+
+            GUIMenu.CycleColors(GUIStyle);
+            GUIMenu.CycleColors(backgroundStyle, true);
+        }
+        public override void OnGUI()
+        {
+            if (menuIsEnabled)
+            {
+                GUI.Box(new Rect(0, 0, Screen.width, 30), "");
+                GUI.Box(new Rect(0, 0, Screen.width, 25), "WraithGUI", GUIStyle);
+
+                if (GUI.Button(new Rect(5, 0, 50, 25), "Main", backgroundStyle))
+                {
+                    mainMenuIsEnabled = !mainMenuIsEnabled;
+                }
+
+                if (GUI.Button(new Rect(65, 0, 60, 25), "Maps", backgroundStyle))
+                {
+                    
+                }
+
+                if (GUI.Button(new Rect(135, 0, 65, 25), "Colors", backgroundStyle))
+                {
+
+                }
+
+                if (GUI.Button(new Rect(210, 0, 65, 25), "Players", backgroundStyle))
+                {
+
+                }
+
+                if (mainMenuIsEnabled)
+                {
+                    playerMenuIsEnabled = false;
+                    levelMenuIsEnabled = false;
+                    ghostMenuIsEnabled = false;
+                    GUI.Box(new Rect(0, 31, 150, 250), "");
+
+                    if (GUI.Button(new Rect(20, 40, 100, 25), "Player Menu", backgroundStyle)) { playerMenuIsEnabled = !playerMenuIsEnabled; }
+
+                    if (GUI.Button(new Rect(20, 70, 100, 25), "Ghost Menu", backgroundStyle)) { ghostMenuIsEnabled = !ghostMenuIsEnabled; }
+                    
+                    if (GUI.Button(new Rect(20, 100, 100, 25), "Level Menu", backgroundStyle)) { levelMenuIsEnabled = !levelMenuIsEnabled; }
+                }
+
+                if (playerMenuIsEnabled)
+                {
+                    mainMenuIsEnabled = false;
+                    ghostMenuIsEnabled = false;
+                    levelMenuIsEnabled = false;
+                    GUI.Box(new Rect(0, 31, 150, 250), "");
+                    if (GUI.Button(new Rect(20, 40, 100, 25), "player test", backgroundStyle)) {  }
+                }
+
+                if (ghostMenuIsEnabled)
+                {
+                    mainMenuIsEnabled = false;
+                    playerMenuIsEnabled = false;
+                    levelMenuIsEnabled = false;
+                    GUI.Box(new Rect(0, 31, 150, 250), "");
+                    if (GUI.Button(new Rect(20, 40, 100, 25), "ghost test", backgroundStyle)) {  }
+                }
+
+                if (levelMenuIsEnabled)
+                {
+                    mainMenuIsEnabled = false;
+                    playerMenuIsEnabled = false;
+                    ghostMenuIsEnabled = false;
+                    GUI.Box(new Rect(0, 31, 150, 250), "");
+                    if (GUI.Button(new Rect(20, 40, 100, 25), "level test", backgroundStyle)) {  }
+                }
+            }
+        }
+
+        private void InitializeGameObjects()
+        {
+            levelController = UnityEngine.Object.FindObjectOfType<LevelController>();
+        }
+
+    }
+}
