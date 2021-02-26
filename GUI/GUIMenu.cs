064adc5ea0ecbe5871ba7059db6a8e61773996bd
Karma Kitten <53679096+karmakittenx@users.noreply.github.com>
2021-02-26 18:44:02 -0500

    Added color changing and credits for MakeTex

--- GUI/GUIMenu.cs
+++ GUI/GUIMenu.cs
@@ -5,6 +5,7 @@ namespace WraithGUI
 {
     public sealed class GUIMenu
     {
+        public static string[] colorStrings = { "Red", "Yellow", "Green", "Cyan", "Blue", "Magenta", "White", "Grey", "Black" };
         public static Color[] allColors = { Color.red, Color.yellow, Color.green, Color.cyan, Color.blue, Color.magenta, Color.white, Color.grey, Color.black, };
         private static float tValue;
         public static string ghostText = "";
@@ -17,13 +18,15 @@ namespace WraithGUI
             tValue = (Mathf.Sin(Time.time * 1.5f) + 1) / 2.0f;
             if (!background)
             {
-                guiStyle.normal.textColor = Color.Lerp(Color.cyan, Color.magenta, tValue);
+                guiStyle.normal.textColor = Color.Lerp(allColors[PhasmoGame.primaryDropdownState.Select], allColors[PhasmoGame.secondaryDropdownState.Select], tValue);
             }
             else
             {
-                guiStyle.normal.background = MakeTex(51, 26, Color.Lerp(Color.cyan, Color.magenta, tValue));
+                guiStyle.normal.background = MakeTex(51, 26, Color.Lerp(allColors[PhasmoGame.primaryDropdownState.Select], allColors[PhasmoGame.secondaryDropdownState.Select], tValue));
             }
         }
+
+        // From https://forum.unity.com/threads/change-gui-box-color.174609/
         private static Texture2D MakeTex(int width, int height, Color col)
         {
             Color[] pix = new Color[width * height];
